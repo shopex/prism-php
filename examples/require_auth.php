694ff20c56cb61089dcc2bc2fc5cd69a92d62e80
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-11 17:27:20 +0800

    修改

--- examples/require_auth.php
+++ examples/require_auth.php
@@ -1,7 +1,6 @@
 <?php
 
 require_once('../lib/Prism.php');
-require_once('../lib/Oauth.php');
 
 $client = new Prism($url = 'http://192.168.51.50:8080/api', $key = 'pufy2a7d', $secret = 'skqovukpk2nmdrljphgj');
 
