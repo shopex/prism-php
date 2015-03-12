949a22f833758ccab3d50cd42847832579704986
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-12 11:35:59 +0800

    sign test

---
+++ test/SignTest.php
@@ -0,0 +1,39 @@
+<?php
+
+require_once(__DIR__ . '/TestBase.php');
+require_once('../lib/Sign.php');
+
+class CurlTest extends TestBase  {
+
+    /**
+    * Sign数字签名
+    *
+    * 功能
+    * 对Method，Path, Headers, Query, PostData, Secret进行签名
+    * Headers, Query, PostData需要排序
+    *
+    */
+    public function testSign() {
+        $headers = array(
+            'X_API_UNITTEST1' => 'A',
+            'X_API_UNITTEST2' => 'B'
+        );
+
+        $params1 = array(
+            'param1' =>'C',
+            'param2' =>'D',
+        );
+
+        $params2 = array(
+            'param1' =>'E',
+            'param2' =>'F',
+        );
+
+        $signResult = 'signResult:ERROR';
+        $r = Sign::produce('GET', '/api/test/test', $headers, $params1, $params2, $this->secret);
+        if ($r)
+            $signResult = 'signResult:OK';
+        $this->assertEquals('signResult:OK', $signResult);
+    }
+
+}
