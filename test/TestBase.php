77108066bbd79f115b208a681cf94fef48f40cd4
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-20 14:37:15 +0800

    优化 去除不必要方法和属性

--- test/TestBase.php
+++ test/TestBase.php
@@ -4,14 +4,11 @@ require_once(__DIR__ . '/../src/Prism.php');
 
 class TestBase extends PHPUnit_Framework_TestCase {
 
+    // 请先配置参数
     var $client_id  = 'pufy2a7d';
     var $secret     = 'skqovukpk2nmdrljphgj';
     var $local_url  = 'http://127.0.0.1:8080/api';
     var $remote_url = 'http://192.168.51.50:8080/api';
-
-    // 手动验证Oauth功能时使用
-    var $oauth_url  = '192.168.51.50:8080/oauth/authorize?client_id=pufy2a7d';
-    var $code       = 'c6ynsqci4efkzuvbplpe'; // 请先修改最新的有效code
 
     var $headers = array(
         'X_API_UNITTEST1' => 'A',
@@ -22,12 +19,5 @@ class TestBase extends PHPUnit_Framework_TestCase {
         'param1' =>'C',
         'param2' =>'D',
     );
-
-    function __construct() {
-
-//        $url = str_replace('/api', '', $this->url);
-//        $this->oauth_client = new Prism($url, $this->client_id, $this->secret);
-
-    }
 
 }
