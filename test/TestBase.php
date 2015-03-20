49d4ae08a80bcaa06610cb9971aea0ff8d45239a
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-20 13:49:46 +0800

    区分remote和local url

--- test/TestBase.php
+++ test/TestBase.php
@@ -6,8 +6,8 @@ class TestBase extends PHPUnit_Framework_TestCase {
 
     var $client_id  = 'pufy2a7d';
     var $secret     = 'skqovukpk2nmdrljphgj';
-    var $url        = 'http://192.168.51.50:8080/api';
-//    var $url        = 'http://127.0.0.1:8080/api';
+    var $local_url  = 'http://127.0.0.1:8080/api';
+    var $remote_url = 'http://192.168.51.50:8080/api';
 
     // 手动验证Oauth功能时使用
     var $oauth_url  = '192.168.51.50:8080/oauth/authorize?client_id=pufy2a7d';
@@ -25,8 +25,8 @@ class TestBase extends PHPUnit_Framework_TestCase {
 
     function __construct() {
 
-        $url = str_replace('/api', '', $this->url);
-        $this->oauth_client = new Prism($url, $this->client_id, $this->secret);
+//        $url = str_replace('/api', '', $this->url);
+//        $this->oauth_client = new Prism($url, $this->client_id, $this->secret);
 
     }
 
