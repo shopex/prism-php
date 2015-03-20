ffee80cff835f5db1dd38844c6be65e45719ad34
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-20 15:51:45 +0800

    完善注释

--- examples/get.php
+++ examples/get.php
@@ -1,11 +1,20 @@
 <?php
 require_once(__DIR__.'/../src/Prism.php');
 
+
+// 运行example前先要起个服务器，比如我们用PHP的自带dev-server:
+// php -S 0.0.0.0:8080 test/testserver.php
+
+// 新建对象 填入在Prism平台上注册的信息 本地测试的话随意填写就行了
 $client = new Prism($url = 'http://192.168.51.50:8080/api', $key = 'pufy2a7d', $secret = 'skqovukpk2nmdrljphgj');
 //$client = new Prism($url = 'http://127.0.0.1:8080/api', $key = 'pufy2a7d', $secret = 'skqovukpk2nmdrljphgj');
 
+
+// 设置底层请求方式（默认用curl）
 //$client->setRequester('socket');
+
+// 发起请求
 echo $client->get('/test/test');
 
-// 返回
+// 应该返回
 // {"httpMethod":"GET","responseTime":"10ms"}
