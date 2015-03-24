c3da693c969dfb8422e9d4592d6a1178b82a5344
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-24 17:49:37 +0800

    修改注释

--- examples/notify-consume.php
+++ examples/notify-consume.php
@@ -5,7 +5,7 @@ require_once(__DIR__.'/../src/Prism.php');
 $client = new Prism($url = 'http://192.168.51.50:8080/api', $key = 'pufy2a7d', $secret = 'skqovukpk2nmdrljphgj');
 
 
-// 往消息队列里获取/消费100个元素并确认
+// 往消息队列里获取(消费)元素并确认
 while(1) {
 
     $r = $client->consume();
