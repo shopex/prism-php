d7f7eeb18c0b05295023eab57bff52e62351abac
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-26 17:48:32 +0800

    更新测试

--- examples/rpc.php
+++ examples/rpc.php
@@ -10,41 +10,37 @@ $client = new Prism($url = 'http://192.168.51.50:8080/api', $key = 'pufy2a7d', $
 
 // 准备一些自定义Header信息
 $headers = array(
-    'X_API_UNITTEST1' => 'A',
-    'X_API_UNITTEST2' => 'B'
-);
-
-// 准备一些需要传递给API的参数
-$params = array(
-	'student id' =>'101',
-	'student name' =>'Jason',
+    'X_API_TEST1' => 'A',
+    'X_API_TEST2' => 'B'
 );
 
 // 可以设置使用CURL还是SOCKET请求方式，默认会优先调用CURL方法
 $client->setRequester('socket');
 $client->setRequester('curl');
 
+
+// 可以携带Oauth的Access Token
+//$client->access_token = 'c4t6q5rh6fysu5v5ww5xenv4';
+
 // 发起GET/POST/PUT/DELETE请求
 $result = array();
 
-$result['GET']      = $client->get('/university/student', $params, $headers);
-$result['POST']     = $client->post('/university/student', $params, $headers);
-$result['PUT']      = $client->put('/university/student', $params, $headers);
-$result['DELETE']   = $client->delete('/university/student', $params, $headers);
 
-print_r($result);
+// 通过id获取学生信息 (GET请求)
+$params = array(
+	'id' =>'43',
+);
+$r = $client->get('/university/student', $params, $headers);
+echo $r."\n";
+// 返回结果：{"jsonrpc":"2.0","result":{"id":43,"name":"Rose DeWitt Bukater","age":17,"desc":"Is forced into an engagement to 30-year-old Cal Hockley."},"id":"fhildwpktl7ix4f2"}
 
-/*
-返回：
-Array
-(
-    [GET] => {"httpMethod":"GET","header1":"A","header2":"B","query":{"param1":"C","param2":"D","param3":"E","param4":"F"},"responseTime":"10ms"}
 
-    [POST] => {"httpMethod":"POST","header1":"A","header2":"B","data":{"param1":"C","param2":"D","param3":"E","param4":"F"},"responseTime":"10ms"}
-
-    [PUT] => {"httpMethod":"PUT","header1":"A","header2":"B","data":{"param1":"C","param2":"D","param3":"E","param4":"F"},"responseTime":"10ms"}
-
-    [DELETE] => {"httpMethod":"DELETE","header1":"A","header2":"B","query":{"param1":"C","param2":"D","param3":"E","param4":"F"},"responseTime":"10ms"}
-
-)
-*/
+// 创建一个学生 (POST请求)
+$params = array(
+    'name'=>'Caledon Nathan',
+    'age'=>30,
+    'desc'=>"Cal is Rose's 30-year-old fiancé.",
+);
+$r = $client->post('/university/student', $params, $headers);
+echo $r."\n";
+// 返回结果：{"jsonrpc":"2.0","error":{"code":"-32600","message":"Invalid Request","data":"Oauth is required"},"id":"ejscogtwqw3gz3px"}
