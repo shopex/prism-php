0727db9a08121feeba0d967d18653004c05064d4
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-26 17:48:45 +0800

    更新

--- examples/server.php
+++ examples/server.php
@@ -1,37 +1,72 @@
 <?php
-// 先起服务器
-// php -S 0.0.0.0:8080 server.php
-
+// 先起服务器 php -S 0.0.0.0:8080 server.php
 require_once(__DIR__.'/../src/Provider.php');
 
-$server = new Provider();
 
-$server->get( '/student', new Api(), 'fetchStudent' );
-$server->post( '/student', new Api(), 'createStudent' );
-$server->put( '/student', new Api(), 'modifyStudent' );
-$server->delete( '/student', new Api(), 'deleteStudent' );
+$server = new Provider(); // 创建服务端Provider实例
+$api    = new Api(); // 创建API处理器(handler/controller)实例
+
+/**
+* 添加路由，Provider会去寻找API下的fetchStudent方法来处理请求
+* $method_id/path 客户端请求里携带的参数，这里为id=>43
+* $api API处理器(handler/controller)的实例
+* $action/$function_name 需要调用的处理方法
+* $require_oauth: 是否需要oauth验证(默认为false)
+*/
+$server->get( '/student', $api, 'fetchStudent', false );
 
 
+$server->post( '/student', $api, 'createStudent', true );
+$server->put( '/student', $api, 'modifyStudent' );
+$server->delete( '/student', $api, 'deleteStudent' );
+
+
+// API处理器(handler/controller) 可以自己定义具体的名称
 class Api {
 
-    function fetchStudent($param) {
-        echo "fetchStudent:";
-        echo json_encode($param);
+    var $model;
+
+    // 为测试方便添加的模型层和测试数据
+    function __construct () {
+        $this->model = new Model();
     }
 
-    function createStudent($param) {
-        echo "createStudent:";
-        echo json_encode($param);
+    /**
+    * $params 客户端请求里携带的参数，这里为id=>43
+    * $headers 客户端请求里携带的自定义头信息
+    * $request_id Prism返回的request id
+    * $oauth_info 客户端请求里携带的oauth头信息
+    * $response 用于返回标准化的JSONRPC 2.0的JSON格式响应
+    */
+    function fetchStudent($params, $headers, $request_id, $oauth_info, $response) {
+        $response->send('success', $this->model->students[$params['id']]);
     }
 
-    function modifyStudent($param) {
-        echo "modifyStudent:";
-        echo json_encode($param);
+    function createStudent($params, $headers, $request_id, $oauth_info, $response) {
+        $response->send('success', 'student:44 is created');
     }
 
-    function deleteStudent($param) {
-        echo "deleteStudent:";
-        echo json_encode($param);
-    }
+}
 
+
+// 为测试方便添加的模型层和测试数据
+class Model {
+    var $students = array();
+
+    function __construct () {
+
+        $this->students[42] = array(
+            'id'=>42,
+            'name'=>'Jack Dawson',
+            'age'=>21,
+            'desc'=>'A penniless artist.',
+        );
+        $this->students[43] = array(
+            'id'=>43,
+            'name'=>'Rose DeWitt Bukater',
+            'age'=>17,
+            'desc'=>'Is forced into an engagement to 30-year-old Cal Hockley.',
+        );
+
+    }
 }
--- src/Provider.php
+++ src/Provider.php
@@ -1,12 +1,25 @@
 <?php
+require_once(__DIR__ . '/../modules/Response.php');
+require_once(__DIR__ . '/../modules/PrismException.php');
+
 class Provider {
 
+    var $remote_arr;
     var $method;
     var $path;
-    var $query = array();
-    var $body = array();
+    var $caller_ip;
+    var $request_id;
+    var $query      = array();
+    var $post_data  = array();
+    var $params     = array();
+    var $headers    = array();
+    var $app_info   = array();
+    var $oauth_info = array();
 
     public function __construct () {
+
+        // prepare remote address
+        $this->remote_arr = $_SERVER['REMOTE_ADDR'];
 
         // prepare method
         $this->method = $_SERVER['REQUEST_METHOD'];
@@ -19,34 +32,121 @@ class Provider {
         if ( isset($url_arr['query']) )
             $this->query = $this->queryToArray($url_arr['query']);
 
-        // prepare body
-        $this->body = $this->queryToArray(file_get_contents('php://input'));
+        // prepare post_data
+        $this->post_data = $this->queryToArray(file_get_contents('php://input'));
+
+        // prepare params
+        $this->params = array_merge($this->query, $this->post_data);
+
+        // prepare headers
+        foreach($_SERVER as $key=>$value) {
+            if ( substr($key, 0, 7) == 'HTTP_X_')
+                $this->headers[str_replace('HTTP_X_', '', $key)] = $value;
+        }
+
+        if ( isset($this->headers['API_ARG']) ) {
+            parse_str($this->headers['API_ARG'], $this->app_info);
+            unset($this->headers['API_ARG']);
+        }
+
+        if ( isset($this->headers['API_OAUTH']) ) {
+            parse_str($this->headers['API_OAUTH'], $this->oauth_info);
+            unset($this->headers['API_OAUTH']);
+        }
+
+        if ( isset($this->headers['CALLER_IP']) ) {
+            $this->caller_ip = $this->headers['CALLER_IP'];
+            unset($this->headers['CALLER_IP']);
+        }
+
+        if ( isset($this->headers['REQUEST_ID']) ) {
+            $this->request_id = $this->headers['REQUEST_ID'];
+            unset($this->headers['REQUEST_ID']);
+        }
+
+        // local fix
+        $this->localFix();
 
     }
 
-    public function get($path, $api, $function_name) {
-        $this->receive('GET', $path, $api, $function_name);
+    // GET快捷方法
+    public function get($path, $api, $function_name, $require_oauth = false) {
+        $this->receive('GET', $path, $api, $function_name, $require_oauth);
     }
 
-    public function post($path, $api, $function_name) {
-        $this->receive('POST', $path, $api, $function_name);
+    // POST快捷方法
+    public function post($path, $api, $function_name, $require_oauth = false) {
+        $this->receive('POST', $path, $api, $function_name, $require_oauth);
     }
 
-    public function put($path, $api, $function_name) {
-        $this->receive('PUT', $path, $api, $function_name);
+    // PUT快捷方法
+    public function put($path, $api, $function_name, $require_oauth = false) {
+        $this->receive('PUT', $path, $api, $function_name, $require_oauth);
     }
 
-    public function delete($path, $api, $function_name) {
-        $this->receive('DELETE', $path, $api, $function_name);
+    // DELETE快捷方法
+    public function delete($path, $api, $function_name, $require_oauth = false) {
+        $this->receive('DELETE', $path, $api, $function_name, $require_oauth);
     }
 
-    private function receive ($method, $path, $api, $function_name) {
+    /**
+    * 接受请求
+    * $method: 请求方法 (GET/POST/PUT/DELETE)
+    * $path: 路由/method_id
+    * $api: api对象(处理器)
+    * $function_name: api对象处理方法
+    * $require_oauth: 是否需要oauth验证
+    */
+    private function receive ($method, $path, $api, $function_name, $require_oauth = false) {
 
-        if ($method == $this->method && $path == $this->path && is_callable(array($api, $function_name)) )
-            call_user_func( array($api, $function_name), array_merge($this->query, $this->body) );
+        $response = new Response($this->request_id);
+
+        // method判断
+        if($method != $this->method)
+            return;
+
+        // path判断
+        if($path != $this->path)
+            return;
+
+        // api对象(处理器)判断
+        if( !is_object($api) )
+            $response->send('error', 'Method not found', 'Handling object is not found on server');
+
+        // 处理方法判断
+        if ( !is_callable(array($api, $function_name)) )
+            $response->send('error', 'Method not found', 'Handling action is not found on server');
+
+        // oauth判断
+        if ( $require_oauth && empty($oauth_info) )
+            $response->send('error', 'Invalid Request', 'Oauth is required');
+
+        call_user_func(
+            array($api, $function_name),
+            $this->params, $this->headers, $this->request_id, $this->oauth_info, $response
+        );
 
     }
 
+    // local fix 单元测试时本地连接本地(不经过Prism服务器)时使用
+    private function localFix () {
+
+        if ($this->remote_arr != '127.0.0.1')
+            return;
+
+        // 清理path
+        $this->path = preg_replace("/^\/[^\/]+\/[^\/]+/", '', $this->path);
+
+        // 清理params
+        unset($this->params['client_id']);
+        unset($this->params['sign_method']);
+        unset($this->params['sign_time']);
+        unset($this->params['sign']);
+
+    }
+
+
+    // query to array
     private function queryToArray($query) {
 
         if(empty($query))
