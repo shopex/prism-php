c8460396f0fc478fdb717b439a69b9ad53a5180d
蔡昀辰 <caiyunchen@shopex.cn>
2015-03-31 14:20:04 +0800

    ~

--- examples/get.php
+++ examples/get.php
@@ -3,7 +3,7 @@ require_once(__DIR__.'/../src/PrismClient.php');
 
 
 // 运行example前先要起个服务器，比如我们用PHP的自带dev-server:
-// php -S 0.0.0.0:8080 test/testserver.php
+// php -S 0.0.0.0:8080 example/server-router.php
 
 // 新建对象 填入在Prism平台上注册的信息 本地测试的话随意填写就行了
 $client = new PrismClient($url = 'http://192.168.51.50:8080/api', $key = 'pufy2a7d', $secret = 'skqovukpk2nmdrljphgj');
