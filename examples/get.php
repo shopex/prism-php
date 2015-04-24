e9cafbd6ef35eb6f86277303c3a8d1345bc7542d
jiaolei <jiaolei@shopex.cn>
2015-04-24 14:21:50 +0800

    readme.md

--- examples/get.php
+++ examples/get.php
@@ -7,7 +7,8 @@ require_once(__DIR__.'/../src/PrismClient.php');
 
 
 // 新建对象 填入在Prism平台上注册的信息 本地测试的话随意填写就行了
-$client = new PrismClient($url = 'http://default.local:8080/api', $key = 'pufy2a7d', $secret = 'skqovukpk2nmdrljphgj', "unix:///tmp/api_provider.sock");
+// 第四个参数 $socket socket文件地址，如果有则优先选择socke方式
+$client = new PrismClient($url = 'http://default.local:8080/api', $key = 'pufy2a7d', $secret = 'skqovukpk2nmdrljphgj', $socket = "unix:///tmp/api_provider.sock");
 
 
 // 发起请求
